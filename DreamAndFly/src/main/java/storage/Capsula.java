108d8ed357897f715543ce14b5b4a9b5bd4eb6b4
Valentina Paolillo <127320887+PaoVale@users.noreply.github.com>
2024-02-28 15:25:37 +0100

    inizio capsula e capsula dao

--- DreamAndFly/src/main/java/storage/Capsula.java
+++ DreamAndFly/src/main/java/storage/Capsula.java
@@ -1,5 +1,65 @@
 package storage;
 
 public class Capsula {
+	private Integer id;
+	private float prezzo_orario;
+	private String tipologia;
+	
+	
+	
+	public Capsula(Integer id, float prezzo_orario, String tipologia) {
+		super();
+		this.id = id;
+		this.prezzo_orario = prezzo_orario;
+		this.tipologia = tipologia;
+	}
 
+
+
+	public Capsula() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public float getPrezzo_orario() {
+		return prezzo_orario;
+	}
+
+
+
+	public void setPrezzo_orario(float prezzo_orario) {
+		this.prezzo_orario = prezzo_orario;
+	}
+
+
+
+	public String getTipologia() {
+		return tipologia;
+	}
+
+
+
+	public void setTipologia(String tipologia) {
+		this.tipologia = tipologia;
+	}
+
+	
+
+	
+	
+	
 }
--- DreamAndFly/src/main/java/storage/CapsulaDao.java
+++ DreamAndFly/src/main/java/storage/CapsulaDao.java
@@ -1,5 +1,84 @@
 package storage;
 
-public class CapsulaDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.sql.DataSource;
+
+public class CapsulaDao {
+private DataSource ds=null;
+	
+	private static final Logger logger = Logger.getLogger(CapsulaDao.class.getName());
+
+	public CapsulaDao(DataSource ds) {
+		super();
+		this.ds=ds;
+	}
+	
+	public Capsula doRetrieveByKey(Integer id) throws SQLException {
+		ResultSet rs;
+		String query;
+		PreparedStatement pst=null;
+		Connection con=null;
+		Capsula capsula=new Capsula();
+		try {
+			con=ds.getConnection();
+			query = "select * from capsula where id = ? ";
+			pst = con.prepareStatement(query);
+			pst.setInt(1, id);
+			rs = pst.executeQuery();
+
+			if(rs.next()) {
+				
+				capsula.setId(rs.getInt("id"));
+				capsula.setPrezzo_orario(rs.getFloat("prezzo_orario"));
+				capsula.setTipologia(rs.getString("tipologia"));
+
+			}
+
+		}catch(Exception e) {
+			logger.log(Level.SEVERE, e.getMessage());
+			logger.log(Level.SEVERE , e.getMessage());
+		} finally {
+			try {
+				if(pst != null)
+					pst.close();
+			}finally{
+				if(con != null)
+					con.close();
+			}
+			
+			
+		}
+		return capsula;
+
+	}
+	
+	public synchronized void doUpdatePrezzoOrario(Integer id, float prezzo_orario) throws SQLException {
+		String query;
+		PreparedStatement pst=null;
+		Connection con=null;
+		try {
+			con=ds.getConnection();
+			query = "update capsula set prezzo_orario = ? where id = ? ";
+			pst = con.prepareStatement(query);
+			pst.setFloat(1, prezzo_orario);
+			pst.setInt(2, id);
+			pst.executeUpdate();
+		}finally {
+			try {
+				if(pst != null)
+					pst.close();
+			}finally{
+				if(con != null)
+					con.close();
+			}
+	}
+	}
+	
+	
 }
