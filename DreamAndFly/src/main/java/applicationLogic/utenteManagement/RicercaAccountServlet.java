fbda4982ffa43896b9a4b199531cbc8f756bd7b2
Amandeep Singh <amandeep04singh@gmail.com>
2024-02-28 15:20:26 +0100

    Implementazione barra di ricerca per cerca Account e creazione servlet elimina account

--- DreamAndFly/src/main/java/applicationLogic/utenteManagement/RicercaAccountServlet.java
+++ DreamAndFly/src/main/java/applicationLogic/utenteManagement/RicercaAccountServlet.java
@@ -16,19 +16,21 @@ import javax.sql.DataSource;
 import storage.AccountUserDao;
 
 /**
- * Servlet implementation class VisualizzaAccountServlet
+ * Servlet implementation class RicercaAccountServlet
  */
-@WebServlet("/VisualizzaAccountServlet")
-public class VisualizzaAccountServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+@WebServlet("/RicercaAccountServlet")
+public class RicercaAccountServlet extends HttpServlet {
 	private static Logger logger = Logger.getAnonymousLogger();
-    
-    public VisualizzaAccountServlet() {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RicercaAccountServlet() {
         super();
-        
+        // TODO Auto-generated constructor stub
     }
 
-	
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		doPost(request, response);
@@ -38,20 +40,20 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
 		AccountUserDao tool=new AccountUserDao(ds);
-		
+		String email = (String)request.getParameter("utente");
+		if(!email.equals("all")) {
 		try {
-			request.setAttribute("listaUtenti", tool.doRetriveAll() );
-			request.setAttribute("listaUtentiPerSelect", tool.doRetriveAll() );
-			
+			request.setAttribute("type", "elemento");
+			request.setAttribute("user", tool.doRetrieveByKey(email));
 		}catch (SQLException e){
 			logger.log(Level.WARNING, "Problema Sql!",e);
 		}
+	
 		
 		
+	}
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Interface/GestoreGUI/GestoreAccount/Visualizza-EliminaAccount.jsp");
 	    dispatcher.forward(request, response);
 	}
 
-
-
-	}
+}
