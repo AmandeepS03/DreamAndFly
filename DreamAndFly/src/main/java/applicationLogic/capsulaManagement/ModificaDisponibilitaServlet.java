af95c7764c279c7addcfa51e6fb610f6b530977b
Amandeep Singh <amandeep04singh@gmail.com>
2024-03-02 16:43:21 +0100

    Implementazione modifica disponibilità

---
+++ DreamAndFly/src/main/java/applicationLogic/capsulaManagement/ModificaDisponibilitaServlet.java
@@ -0,0 +1,87 @@
+package applicationLogic.capsulaManagement;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import storage.PrenotabileDao;
+import storage.Prenotabile;
+
+
+/**
+ * Servlet implementation class ModificaDisponibilitàServlet
+ */
+@WebServlet("/ModificaDisponibilitaServlet")
+public class ModificaDisponibilitaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static Logger logger = Logger.getAnonymousLogger();
+
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModificaDisponibilitaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
+		PrenotabileDao tool=new PrenotabileDao(ds);
+		Prenotabile prenotabile = new Prenotabile();
+//		Date data = Date.valueOf(request.getParameter("data"));
+		String data = request.getParameter("data");		
+		Integer id = Integer.valueOf(request.getParameter("numero"));
+		Integer fascia = Integer.valueOf(request.getParameter("fasciaOraria"));
+		String disponibileValue = request.getParameter("disponibile");
+
+		
+		if(disponibileValue.equals("no")) {
+		try {
+			tool.doDelete(data,id,fascia);
+			
+			
+		}catch (SQLException e){
+			logger.log(Level.WARNING, "Problema Sql!",e);
+		}
+		}else if(disponibileValue.equals("si")) {
+			try {
+				prenotabile.setDataPrenotabile(data);
+				prenotabile.setCapsulaId(id);
+				prenotabile.setFasciaOrariaNumero(fascia);
+				tool.doSave(prenotabile);
+				
+				
+			}catch (SQLException e){
+				logger.log(Level.WARNING, "Problema Sql!",e);
+			}
+			}
+		
+
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Interface/GestoreGUI/GestoreCapsule/ModificaDisponibilità.jsp");
+		    dispatcher.forward(request, response);
+		
+	}
+	
+
+}
--- DreamAndFly/src/main/java/storage/PrenotabileDao.java
+++ DreamAndFly/src/main/java/storage/PrenotabileDao.java
@@ -1,6 +1,7 @@
 package storage;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -136,6 +137,32 @@ public class PrenotabileDao {
 			pst.setString(1, prenotabile.getDataPrenotabile());
 			pst.setInt(2, prenotabile.getCapsulaId());
 			pst.setInt(3, prenotabile.getFasciaOrariaNumero());
+			
+			pst.executeUpdate();
+		}finally {
+			try {
+				if(pst != null)
+					pst.close();
+			}finally{
+				if(con != null)
+					con.close();
+			}
+		}
+	}
+	
+public void doDelete(String data, int id, int fasciaOraria) throws SQLException {
+		
+		String query;
+		PreparedStatement pst=null;
+		Connection con=null;
+		try {
+			con=ds.getConnection();
+			query="delete from e_prenotabile where data_prenotabile = ? and capsula_id = ? and fascia_oraria_numero = ?";
+			con.setAutoCommit(true);
+			pst = con.prepareStatement(query);
+			pst.setString(1, data);
+			pst.setInt(2, id);
+			pst.setInt(3, fasciaOraria);
 			
 			pst.executeUpdate();
 		}finally {
